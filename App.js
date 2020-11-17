defd23a204d18fb4e712fe2c4f03b38fdbbf21f7
E7450 <e7450@E7450s-MacBook-Pro.local>
2020-11-17 08:43:25 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, StatusBar} from 'react-native';
+import 'react-native-gesture-handler';
+
+const App = () => {
+  return (
+    <>
+      <Text>home</Text>
+      <Text>home</Text>
+    </>
+  );
+};
+
+export default App;
---
+++ navgation/navgation.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
